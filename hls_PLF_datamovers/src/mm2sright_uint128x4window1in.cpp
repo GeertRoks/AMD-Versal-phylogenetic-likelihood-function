a85320d97e9211c0bbfcddfb946b9aca100fe354
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2024-07-04 10:44:45 +0200

    Add transpose on read to window1in and window1inEV

--- hls_PLF_datamovers/src/mm2sright_uint128x4window1in.cpp
+++ hls_PLF_datamovers/src/mm2sright_uint128x4window1in.cpp
@@ -8,24 +8,10 @@ SPDX-License-Identifier: X11
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 #include <stdio.h>
+#include "transpose.h"
 
 
 extern "C" {
-
-  // Function to transpose a 4x4 matrix represented as a single ap_uint<512> element
-  void transposeRight(ap_uint<512> &input) {
-    ap_uint<32> temp = 0;
-    int idx1, idx2 = 0;
-    for(int i = 0; i < 4; i++) {
-      for (int j = i+1; j < 4; j++) {
-        idx1 = (i*4+j)*32;
-        idx2 = (j*4+i)*32;
-        temp = input.range(idx2 + 31, idx2);
-        input.range(idx2 + 31, idx2) = input.range(idx1 + 31, idx1);
-        input.range(idx1 + 31, idx1) = temp;
-      }
-    }
-  }
 
   void mm2sright(ap_uint<512>* mem, unsigned int alignment_sites, unsigned int window_size, hls::stream<ap_axiu<128,0,0,0>> &s0, hls::stream<ap_axiu<128,0,0,0>> &s1, hls::stream<ap_axiu<128,0,0,0>> &s2, hls::stream<ap_axiu<128,0,0,0>> &s3) {
 #pragma HLS INTERFACE m_axi port=mem offset=slave bundle=gmem
@@ -40,11 +26,15 @@ extern "C" {
 #pragma HLS INTERFACE s_axilite port=window_size bundle=control
 #pragma HLS interface s_axilite port=return bundle=control
 
-    ap_uint<512> buffer = 0;
-    //hls::stream<ap_axiu<128,0,0,0>>* data_streams[] = {&s0, &s1, &s2, &s3};
-    ap_axiu<128,0,0,0> x;
+    // read branch matrices and transpose them
+    ap_uint<512> branchleft[4] = {0,0,0,0};
+    transpose(mem[0], branchleft[0]);
+    transpose(mem[1], branchleft[1]);
+    transpose(mem[2], branchleft[2]);
+    transpose(mem[3], branchleft[3]);
 
-    const unsigned int alignments_per_window = (window_size>>4); // (window_size/4 bytes per value)/4 values per window
+    // (window_size/4 bytes per value)/4 values per alignment -> div by 16
+    const unsigned int alignments_per_window = (window_size>>4);
     const unsigned int num_windows = (alignment_sites+alignments_per_window-1)/alignments_per_window;
 
     // calculate if alignment sites fits in the windows or if extention by zeroes is needed
@@ -56,28 +46,24 @@ extern "C" {
 
       // Split the branch matrix and prepend them to each data stream
       // (64 elem * 32 = 2048 bits / 512 bits = 4 mem reads with 4 128-bit stream writes each (16 total))
-      buffer = mem[0];
-      transposeRight(buffer);
       for(unsigned int j = 0; j < 4; j++) {
-        x.data = buffer.range(127 + j*128, j*128);
+        ap_axiu<128,0,0,0> x;
+        x.data = branchleft[0].range(127 + j*128, j*128);
         s0.write(x);
       }
-      buffer = mem[1];
-      transposeRight(buffer);
       for(unsigned int j = 0; j < 4; j++) {
-        x.data = buffer.range(127 + j*128, j*128);
+        ap_axiu<128,0,0,0> x;
+        x.data = branchleft[1].range(127 + j*128, j*128);
         s1.write(x);
       }
-      buffer = mem[2];
-      transposeRight(buffer);
       for(unsigned int j = 0; j < 4; j++) {
-        x.data = buffer.range(127 + j*128, j*128);
+        ap_axiu<128,0,0,0> x;
+        x.data = branchleft[2].range(127 + j*128, j*128);
         s2.write(x);
       }
-      buffer = mem[3];
-      transposeRight(buffer);
       for(unsigned int j = 0; j < 4; j++) {
-        x.data = buffer.range(127 + j*128, j*128);
+        ap_axiu<128,0,0,0> x;
+        x.data = branchleft[3].range(127 + j*128, j*128);
         s3.write(x);
       }
 
@@ -85,6 +71,7 @@ extern "C" {
       for(unsigned int i = 0; i < alignments_per_window; i++) {
         if (window >= num_windows-1 && remainder > 0 && i >= remainder) {
           // add zeroes to the input stream if alignments does not fit exactly in the last aie window
+          ap_axiu<128,0,0,0> x;
           x.data = 0;
           s0.write(x);
           s1.write(x);
@@ -93,8 +80,9 @@ extern "C" {
         } else {
           // read all 16 values of one alignement
           // ((512 bits read/8 bits per byte)/4 bytes per float = 16 values)
-          buffer = mem[4+(alignments_per_window*window)+i];
+          ap_uint<512> buffer = mem[4+(alignments_per_window*window)+i];
           // give each data stream 4 data values of the 16 over a 128-bit stream ((128/8)/4 = 4 values)
+          ap_axiu<128,0,0,0> x;
           x.data = buffer.range(127, 0);
           s0.write(x);
           x.data = buffer.range(255, 128);
